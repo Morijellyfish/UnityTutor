11de472a52f5d64078c7269b2dcc5540f96f35f4
s181034-MoriJellyfish <52020604+s181034-MoriJellyfish@users.noreply.github.com>
2021-01-08 16:13:40 +0900

    ugoku

--- Assets/Scripts/AddRForce.cs
+++ Assets/Scripts/AddRForce.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class AddRForce : MonoBehaviour
 {
+    [SerializeField] Vector3 force;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<Rigidbody>().AddRelativeForce(force);
     }
 
     // Update is called once per frame
