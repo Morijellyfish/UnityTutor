181256e30b714613c2a0097beed4aed91a4f2fab
Yakisika0706 <70683353+Yakisika0706@users.noreply.github.com>
2021-01-28 17:23:24 +0900

    move prayer

---
+++ 39/prayermove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prayermove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(2f * Time.deltaTime, 0f, 0f);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+
+            transform.Translate(-2f * Time.deltaTime, 0f, 0f);
+        }
+    }
+   
+}
