dc42bc70077159db2badcc806d06d12f778d0634
Yakisika0706 <70683353+Yakisika0706@users.noreply.github.com>
2021-01-28 17:39:27 +0900

    Ontriggarstay　理解編

---
+++ 39/Cubeup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubeup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerStay(Collider Player)
+    {
+        //範囲にいるときにW押すと上に上がる
+       if (Input.GetKey(KeyCode.W))
+          {
+             transform.Translate(0f, 2f * Time.deltaTime, 0f);
+          }
+    }
+}
